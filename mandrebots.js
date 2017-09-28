aa4c6c24958f5e7a96c9689068ab3d47be9eb04e
Anwesh43 <anweshthecool0@gmail.com>
2017-09-28 11:46:12 +0530

    calculating mandrebot

--- mandrebots.js
+++ mandrebots.js
@@ -3,10 +3,35 @@ class Complex {
         this.re = re
         this.im = im
     }
+    square() {
+        return Complex.multiply(this,this)
+    }
+    magnitude() {
+        return Math.sqrt(Math.pow(this.re,2)+Math.pow(this.im,2))
+    }
     static multiply(c1,c2) {
         return new Complex(c1.re*c2.re-c1.im*c2.im,(c1.re*c2.im)+(c1.im*c2.re))
     }
     static add(c1,c2) {
         return new Complex(c1.re+c2.re,c1.im+c2.im)
+    }
+}
+class MandrebotSet {
+    constructor(n,lastVal) {
+        this.n = n
+        this.lastVal = lastVal
+    }
+    calculateMandrebot(c) {
+        var i = 0
+        var currComplexNum = c
+        while(i < n) {
+            var sq = currComplexNum.square()
+            if(sq.magnitude() >= this.lastVal) {
+                break
+            }
+            currComplexNum = Complex.add(currComplexNum,sq)
+            i++
+        }
+        return Math.floor(255*(i)/100)
     }
 }
