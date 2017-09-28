d2b86e60307a8de69d9fa68b5237201923362072
Anwesh43 <anweshthecool0@gmail.com>
2017-09-28 12:10:35 +0530

    adding canvas util methods

--- mandrebots.js
+++ mandrebots.js
@@ -35,3 +35,16 @@ class MandrebotSet {
         return Math.floor(255*(i)/100)
     }
 }
+const createCanvas = (w,h) => {
+    window.canvas = document.createElement('canvas')
+    canvas.width = w
+    canvas.height = h
+    window.context = canvas.getContext('2d')
+}
+const getPixels = () => {
+    window.imData = window.context.getImageData(0,0,window.canvas.width,window.canvas.height)
+    return imData.data
+}
+const setPixels = () => {
+    window.context.setImageData(window.imData,0,0)
+}
